5e0ab1c32c4aab2403720b95a32ab54edc11872d
manh1122005 <voidoistaken@gmail.com>
2026-07-27 00:39:27 +0700

    Revert "init"
    This reverts commit 080f6d5f7cfe2bde2588ae8da416f648229115ab.

--- gmtk-1/script/menu.gd
+++ gmtk-1/script/menu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-const GAME_SCENE_PATH := "res://screen/map1.tscn"
 
 
 
@@ -10,9 +9,8 @@ func _on_button_3_pressed() -> void:
 
 
 func _on_button_pressed() -> void:
-	var result := get_tree().change_scene_to_file("res://screen/level selection.tscn")
-	if result != OK:
-		push_error("Menu: failed to open %s (error %d)." % [GAME_SCENE_PATH, result])
+	get_tree().change_scene_to_file("res://screen/map1.tscn")
+	pass # Replace with function body.
 
 
 func _on_button_2_pressed() -> void:
