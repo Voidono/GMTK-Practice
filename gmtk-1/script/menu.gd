080f6d5f7cfe2bde2588ae8da416f648229115ab
manh1122005 <voidoistaken@gmail.com>
2026-07-27 00:09:01 +0700

    init

--- gmtk-1/script/menu.gd
+++ gmtk-1/script/menu.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const GAME_SCENE_PATH := "res://screen/map1.tscn"
 
 
 
@@ -9,8 +10,9 @@ func _on_button_3_pressed() -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://screen/map1.tscn")
-	pass # Replace with function body.
+	var result := get_tree().change_scene_to_file("res://screen/level selection.tscn")
+	if result != OK:
+		push_error("Menu: failed to open %s (error %d)." % [GAME_SCENE_PATH, result])
 
 
 func _on_button_2_pressed() -> void:
